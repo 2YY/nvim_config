2fcb028e0b320626e19b4d0a183af9770ec58a59
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2025-04-01 22:49:53 +0900

    feat: 使えなくなったコマンドやプロンプトを整理

--- lua/plugins/user_copilot_chat.lua
+++ lua/plugins/user_copilot_chat.lua
@@ -24,10 +24,13 @@ return {
     keys = {
       { "<Leader><Leader>", ":CopilotChat ", desc = "CopilotChat", mode = {"n", "v"} },
       { "<Leader>1", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle Vsplit" },
-      { "<Leader>2", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix diagnostic", mode = {"n", "v"} },
+      { "<Leader>2", "<cmd>CopilotChatExplain<cr>", desc = "Write an explanation for the selected code", mode = {"n", "v"} },
       { "<Leader>3", "<cmd>CopilotChatDocs<cr>", desc = "CopilotChat - Add documentation comment", mode = {"n", "v"} },
       { "<Leader>4", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests", mode = {"n", "v"} },
-      { "<Leader>5", "<cmd>CopilotChatCommitStaged<cr>", desc = "CopilotChat - Write commit message for staged files" },
+
+      -- FIXME: discontinued 空きスロットとして、今後何か使えそうなものが増えたら使う。別の機能に割り振ってもいい。
+      -- { "<Leader>5", "<cmd>CopilotChatCommitStaged<cr>", desc = "CopilotChat - Write commit message for staged files" },
+
       { "<Leader>7", "<cmd>CopilotChatFix<cr>", desc = "CopilotChat - Fix", mode = {"n", "v"} },
       { "<Leader>8", "<cmd>CopilotChatOptimize<cr>", desc = "CopilotChat - Optimize", mode = {"n", "v"} },
       { "<Leader>9", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review", mode = {"n", "v"} },

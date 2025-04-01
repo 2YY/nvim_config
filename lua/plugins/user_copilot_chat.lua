71d6abec24a882c491769c82fb527621ba28e954
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2025-04-01 22:57:27 +0900

    feat: copilot系のマッピング調整

--- lua/plugins/user_copilot_chat.lua
+++ lua/plugins/user_copilot_chat.lua
@@ -22,18 +22,23 @@ return {
     end,
     event = "VeryLazy",
     keys = {
+      -- TODO: あまり使ってないので他の何かに割り振っていいかも。(Toggle で開閉してるのでわざわざこっちを使わない)
       { "<Leader><Leader>", ":CopilotChat ", desc = "CopilotChat", mode = {"n", "v"} },
+
+      -- NOTE: Tier SS
       { "<Leader>1", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle Vsplit" },
-      { "<Leader>2", "<cmd>CopilotChatExplain<cr>", desc = "Write an explanation for the selected code", mode = {"n", "v"} },
-      { "<Leader>3", "<cmd>CopilotChatDocs<cr>", desc = "CopilotChat - Add documentation comment", mode = {"n", "v"} },
-      { "<Leader>4", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests", mode = {"n", "v"} },
+      { "<Leader>2", "<cmd>CopilotChatFix<cr>", desc = "CopilotChat - Fix", mode = {"n", "v"} },
+
+      { "<Leader>3", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests", mode = {"n", "v"} },
+
+      -- NOTE: たまにしか使わない。他の何かに割り振ってもいいかもしれない。
+      { "<Leader>4", "<cmd>CopilotChatDocs<cr>", desc = "CopilotChat - Add documentation comment", mode = {"n", "v"} },
+      { "<Leader>5", "<cmd>CopilotChatExplain<cr>", desc = "Write an explanation for the selected code", mode = {"n", "v"} },
+      { "<Leader>6", "<cmd>CopilotChatOptimize<cr>", desc = "CopilotChat - Optimize", mode = {"n", "v"} },
+      { "<Leader>7", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review", mode = {"n", "v"} },
 
       -- FIXME: discontinued 空きスロットとして、今後何か使えそうなものが増えたら使う。別の機能に割り振ってもいい。
-      -- { "<Leader>5", "<cmd>CopilotChatCommitStaged<cr>", desc = "CopilotChat - Write commit message for staged files" },
-
-      { "<Leader>7", "<cmd>CopilotChatFix<cr>", desc = "CopilotChat - Fix", mode = {"n", "v"} },
-      { "<Leader>8", "<cmd>CopilotChatOptimize<cr>", desc = "CopilotChat - Optimize", mode = {"n", "v"} },
-      { "<Leader>9", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review", mode = {"n", "v"} },
+      -- { "<Leader>8", "<cmd>CopilotChatCommitStaged<cr>", desc = "CopilotChat - Write commit message for staged files" },
     },
   }
 }

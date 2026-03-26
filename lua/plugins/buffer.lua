a8f4944f044ca91e58dd2adc951bf9f3cd3ee330
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2026-03-26 12:55:52 +0900

    feat: JABS のアイコンが表示されるように修正

--- lua/plugins/buffer.lua
+++ lua/plugins/buffer.lua
@@ -1,3 +1,19 @@
 return {
-  { "matbme/JABS.nvim", opts = {} },
+  {
+    "matbme/JABS.nvim",
+    opts = {
+      symbols = {
+        current         = "",
+        split           = "",
+        alternate       = "",
+        hidden          = "",
+        locked          = "",
+        ro              = "",
+        edited          = "",
+        terminal        = "",
+        default_file    = "",
+        terminal_symbol = "",
+      },
+    },
+  },
 }
